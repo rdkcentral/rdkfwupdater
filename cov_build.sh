bcb00725ca8e900492f55f47dbce76dd619b5d91
Shibu Kakkoth Vayalambron <shibu.kakkoth@gmail.com>
2025-02-04 10:54:30 -0800

    Reuse dependent components from containers

--- cov_build.sh
+++ cov_build.sh
@@ -4,22 +4,10 @@ export INSTALL_DIR=${ROOT}/local
 mkdir -p $INSTALL_DIR
 cd $ROOT
 
-# Build common utilities
-cd /home
-git clone https://github.com/rdkcentral/common_utilities.git
-cd /home/common_utilities
-autoreconf -i
-./configure --prefix=${INSTALL_DIR} CFLAGS="-Wno-unused-result -Wno-format-truncation -Wno-error=format-security"
-make && make install
+# Below dependencies are available in native build containers 
+# 1] utilities
+# 2] libsyswrapper
 
-#Build libsyswrapper
-cd /home
-git clone https://github.com/rdkcmf/rdk-libSyscallWrapper.git
-cd /home/rdk-libSyscallWrapper
-autoreconf -i
-./configure --prefix=${INSTALL_DIR}
-make
-make install
 
 #Build rdkfwupdater
 cd /home/rdkfwupdater
