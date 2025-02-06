588e4a9267f6bb945bbbcfc9fd440f6a17e0ab9a
Kakkoth Vayalambron, Shibu <shibu.kakkoth@gmail.com>
2025-02-06 14:54:42 -0800

    Include CI workflows for code-coverage in release

--- run_ut.sh
+++ run_ut.sh
@@ -15,6 +15,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+
+ENABLE_COV=false
+
+if [ "x$1" = "x--enable-cov" ]; then
+      echo "Enabling coverage options"
+      export CXXFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+      export CFLAGS="-g -O0 -fprofile-arcs -ftest-coverage"
+      export LDFLAGS="-lgcov --coverage"
+      ENABLE_COV=true
+fi
+
 cd ./unittest/
 
 automake --add-missing
@@ -55,4 +66,16 @@ if [ "$devicestatus" = "0" ] && [ "$deviceutils" = "0" ] && [ "$mainapp" = "0" ]
 else
     echo "L1 UNIT TEST FAILED. PLEASE CHECK AND FIX"
     exit 1
+fi
+
+
+#### Generate the coverage report ####
+if [ "$ENABLE_COV" = true ]; then
+    echo "Generating coverage report"
+    lcov --directory . --capture --output-file coverage.info
+    lcov --remove coverage.info "${PWD}/source/test/*" --output-file coverage.info
+
+    lcov --remove coverage.info "$HOME/usr/*" --output-file coverage.info
+    lcov --remove coverage.info "/usr/*" --output-file coverage.info
+    lcov --list coverage.info
 fi
