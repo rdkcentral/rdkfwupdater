d72c63345f0b99ce6fb1116308f69caa894635a7
adrojaankur <adrojaankur20@gmail.com>
2025-02-25 12:56:29 +0530

    Update mtlsUtils.c

--- src/cedmInterface/mtlsUtils.c
+++ src/cedmInterface/mtlsUtils.c
@@ -22,6 +22,23 @@
 #include "rdkconfig.h"
 #endif
 
+#ifdef RDKCERTSELECTOR
+/* Description: Use for get all mtls related certificate and key.
+ * @param sec: This is a pointer hold the certificate, key and type of certificate.
+ * @return :  MTLS_CERT_FETCH_SUCCESS on success, MTLS_CERT_FETCH_FAILURE on mtls cert failure , STATE_RED_CERT_FETCH_FAILURE on state red cert failure
+ * */
+MtlsAuthStatus getMtlscert(MtlsAuth_t *sec, rdkcertselector_h* pthisCertSel) {
+    /*
+            strncpy(sec->cert_name, STATE_RED_CERT, sizeof(sec->cert_name) - 1);
+	    sec->cert_name[sizeof(sec->cert_name) - 1] = '\0';
+            strncpy(sec->cert_type, "P12", sizeof(sec->cert_type) - 1);
+	    sec->cert_type[sizeof(sec->cert_type) - 1] = '\0';
+            strncpy(sec->key_pas, mtlsbuff, sizeof(sec->key_pas) - 1);
+            sec->key_pas[sizeof(sec->key_pas) - 1] = '\0';
+     * */
+    return MTLS_CERT_FETCH_SUCCESS;
+}
+#else
 /* Description: Use for get all mtls related certificate and key.
  * @param sec: This is a pointer hold the certificate, key and type of certificate.
  * @return : int Success 1 and failure -1
@@ -38,3 +55,4 @@ int getMtlscert(MtlsAuth_t *sec) {
     /* TODO: RDKE-419: temporary change until RDKE-419 gets proper solution. */
     return MTLS_FAILURE;
 }
+#endif
