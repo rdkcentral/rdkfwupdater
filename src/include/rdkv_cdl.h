88702eeba0c75be0f26d9bf94cfc7c947c0d4b92
mkadinti <madhunarasimhulu@gmail.com>
2025-09-10 13:51:11 +0000

    RDK-59064:[RDK-E][POC] Transform rdkfwupgrader into library-driven architecture-libupgrade.so

--- src/include/rdkv_cdl.h
+++ src/include/rdkv_cdl.h
@@ -147,7 +147,8 @@ int fallBack(int server_type, const char* artifactLocationUrl, const void* local
 void uninitialize(int);
 int initialize(void);
 int logFileData(const char *file_path);
-int upgradeRequest(int upgrade_type, int server_type, const char* artifactLocationUrl, const void* dwlloc, char *pPostFields, int *pHttp_code);
+// upgradeRequest() function moved to librdkv_upgrade.so - use rdkv_upgrade_request() from rdkv_upgrade.h instead
+int checkTriggerUpgrade(XCONFRES *pResponse, const char *model);
 int flashImage(const char *server_url, const char *upgrade_file, const char *reboot_flag, const char *proto, int upgrade_type, const char *maint);
 int postFlash(const char *maint, const char *upgrade_file, int upgrade_type, const char *reboot_flag);
 void updateUpgradeFlag(int action);
