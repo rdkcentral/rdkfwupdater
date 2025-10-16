821d3baad173166b313b8aeadf46c855377c002f
KTirumalaSrihari <102281309+KTirumalaSrihari@users.noreply.github.com>
2025-10-16 15:41:53 +0530

    Add getDeviceType function to retrieve device type in rfcinterface.c
    this commit is to add getDeviceType function to retrieve device type via RFC reading

--- src/rfcInterface/rfcinterface.c
+++ src/rfcInterface/rfcinterface.c
@@ -276,3 +276,25 @@ bool isDebugServicesEnabled(void)
     }
     return status;
 }
+
+const char* getDeviceType(void)
+{
+	const char* DeviceType = NULL;
+	int ret = -1;
+	char rfc_data[RFC_VALUE_BUF_SIZE];
+	*rfc_data = 0;
+    ret = read_RFCProperty("LABSGND", RFC_DEVICETYPE, rfc_data, sizeof(rfc_data));
+    if (ret == -1) {
+        SWLOG_ERROR("%s: rfc device type =%s failed status %d \n", __FUNCTION__, RFC_DEVICETYPE, ret);
+	} else {
+	    SWLOG_INFO("%s: rfc device type = %s\n", __FUNCTION__, rfc_data);
+		if ((strncasecmp(rfc_data, "prod", 4)) == 0) { 
+			DeviceType = "prod"; 
+		} else if (strncasecmp(rfc_data, "test", 4) == 0) {
+			DeviceType = "test";
+		} else {
+		    DeviceType = NULL;
+		}
+	}
+	return DeviceType;
+}
