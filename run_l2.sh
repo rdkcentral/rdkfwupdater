19025966493e7ab011f839f35c3fe01eb9d2b964
mkadinti <madhunarasimhulu@gmail.com>
2025-11-04 05:54:30 +0000

    RDK-59276 : [RDK-E] Transform rdkfwupgrader into library-driven architecture- L2 partial fix

--- run_l2.sh
+++ run_l2.sh
@@ -24,6 +24,15 @@ export ROOT=/usr
 export INSTALL_DIR=${ROOT}/local
 mkdir -p $INSTALL_DIR
 
+git clone https://github.com/rdkcentral/common_utilities.git
+ls
+cd common_utilities
+git checkout topic/RDK-59276-modularization
+make && make install
+
+# Return to main rdkfwupdater directory
+cd ..
+
 #Build rdkfwupdater
 autoreconf -i
 ./configure --prefix=${INSTALL_DIR} --enable-rdkcertselector=yes --enable-mountutils=yes --enable-rfcapi=yes CFLAGS="-DRDK_LOGGER"
