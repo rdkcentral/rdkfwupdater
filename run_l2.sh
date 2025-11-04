1adba930cb95ea54b51e446e3d00805f8bccf312
mkadinti <madhunarasimhulu@gmail.com>
2025-11-04 08:36:52 +0000

    RDK-59276 : [RDK-E] Transform rdkfwupgrader into library-driven architecture- L2 partial fix

--- run_l2.sh
+++ run_l2.sh
@@ -24,13 +24,16 @@ export ROOT=/usr
 export INSTALL_DIR=${ROOT}/local
 mkdir -p $INSTALL_DIR
 
-git clone https://github.com/rdkcentral/common_utilities.git
+# Clone common_utilities if not already present
+if [ ! -d "common_utilities" ]; then
+    git clone https://github.com/rdkcentral/common_utilities.git
+fi
 ls
 cd common_utilities
 git checkout topic/RDK-59276-modularization
 # Configure and build common_utilities with proper paths
 autoreconf -i
-./configure --prefix=${INSTALL_DIR}
+./configure --prefix=${INSTALL_DIR} CFLAGS="-Wno-error"
 make && make install
 
 # Return to main rdkfwupdater directory
