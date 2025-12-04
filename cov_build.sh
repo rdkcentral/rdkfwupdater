c6e39a2607796c0b11dc7ade5255907c7abad353
mkadinti <101405874+mkadinti@users.noreply.github.com>
2025-12-04 11:06:50 +0530

    Uncomment git commands in cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -3,11 +3,11 @@ export ROOT=/usr
 export INSTALL_DIR=${ROOT}/local
 mkdir -p $INSTALL_DIR
 
-#git clone https://github.com/rdkcentral/common_utilities.git
-#ls
-#cd common_utilities
-#git checkout topic/RDK-59276-modularization 
-#make && make install
+git clone https://github.com/rdkcentral/common_utilities.git
+ls
+cd common_utilities
+git checkout topic/RDK-59276-modularization 
+make && make install
 
 # Below dependencies are available in native build containers 
 # 1] utilities
