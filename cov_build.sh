904a1771156532c8ea9055c83b01bc0cd9906d2d
mkadinti <madhunarasimhulu@gmail.com>
2025-12-31 10:39:17 +0000

    RDK-60065:[RDK-E] Implement Firmware Download & Install API for Event-Driven Firmware Updates- UpdateFirmware API Implementation- L1 xconf cache load functions- UT compile fail debug

--- cov_build.sh
+++ cov_build.sh
@@ -3,11 +3,11 @@ export ROOT=/usr
 export INSTALL_DIR=${ROOT}/local
 mkdir -p $INSTALL_DIR
 
-#git clone https://github.com/rdkcentral/common_utilities.git
-#ls
-#cd common_utilities
+git clone https://github.com/rdkcentral/common_utilities.git
+ls
+cd common_utilities
 #git checkout topic/RDK-59276-modularization 
-#make && make install
+make && make install
 
 # Below dependencies are available in native build containers 
 # 1] utilities
@@ -16,5 +16,5 @@ mkdir -p $INSTALL_DIR
 
 #Build rdkfwupdater
 autoreconf -i
-./configure --prefix=${INSTALL_DIR} CFLAGS="-DRDK_LOGGER" --enable-dbus-daemon 
+./configure --prefix=${INSTALL_DIR} CFLAGS="-DRDK_LOGGER"
 make && make install
