16790aec4e7888264d250fe6f07b608190d12856
mkadinti <madhunarasimhulu@gmail.com>
2026-01-06 08:01:28 +0000

    RDK-60065:[RDK-E] Implement Firmware Download & Install API for Event-Driven Firmware Updates- UpdateFirmware API Implementation- Adressing Review comments on cov_build.sh

--- cov_build.sh
+++ cov_build.sh
@@ -3,12 +3,6 @@ export ROOT=/usr
 export INSTALL_DIR=${ROOT}/local
 mkdir -p $INSTALL_DIR
 
-#git clone https://github.com/rdkcentral/common_utilities.git
-#ls
-#cd common_utilities
-#git checkout topic/RDK-59276-modularization 
-#make && make install
-
 # Below dependencies are available in native build containers 
 # 1] utilities
 # 2] libsyswrapper
