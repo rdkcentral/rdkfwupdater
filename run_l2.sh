03b5b6127b524d6739a14229388dfea0c0a2a8b7
mkadinti <madhunarasimhulu@gmail.com>
2026-01-16 12:59:39 +0000

    RDK-60065:[RDK-E] Implement Firmware Download & Install API for Event-Driven Firmware Updates- UpdateFirmware API Implementation- L2 for Download Firmware

--- run_l2.sh
+++ run_l2.sh
@@ -73,7 +73,11 @@ pytest --json-report --json-report-file $RESULT_DIR/rdkfwupdater_image_tests.jso
 echo ""
 echo "[2/2] Running D-Bus handler and cache tests..."
 pytest -v -s --json-report --json-report-file $RESULT_DIR/rdkfwupdater_dbus_tests.json \
-       test/functional-tests/tests/test_dbus_RegisterProcess.py
+       test/functional-tests/tests/test_dbus_RegisterProcess.py \
+       test/functional-tests/tests/test_dbus_UnegisterProcess.py \
+       test/functional-tests/tests/test_dbus_CheckForUpdate.py \
+       test/functional-tests/tests/test_dbus_CheckForUpdate_enhanced.py \
+       test/functional-tests/tests/test_dbus_DownloadFirmware.py
 
 echo ""
 echo "=========================================="
