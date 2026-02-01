a8455c0df85104968e47154e2085ba5ac2eecb79
mkadinti <madhunarasimhulu@gmail.com>
2026-02-01 07:18:07 +0000

    RDK-60065:[RDK-E] Implement Firmware Download & Install API for Event-Driven Firmware Updates- UpdateFirmware API Implementation- removed adapted changes from rdk_logger

--- cov_build.sh
+++ cov_build.sh
@@ -3,14 +3,14 @@ export ROOT=/usr
 export INSTALL_DIR=${ROOT}/local
 mkdir -p $INSTALL_DIR
 
-cd /opt/
-git clone https://github.com/rdkcentral/rdk_logger.git
-cd rdk_logger
-autoreconf -i
-./configure
-make & make install
-rm -rf /opt/rdk_logger
-cd ${WORKDIR}
+#cd /opt/
+#git clone https://github.com/rdkcentral/rdk_logger.git
+#cd rdk_logger
+#autoreconf -i
+#./configure
+#make & make install
+#rm -rf /opt/rdk_logger
+#cd ${WORKDIR}
 
 # Below dependencies are available in native build containers 
 # 1] utilities
@@ -19,5 +19,5 @@ cd ${WORKDIR}
 
 #Build rdkfwupdater
 autoreconf -i
-./configure --prefix=${INSTALL_DIR} CFLAGS="-DRDK_LOGGER" --enable-extended-logger
+./configure --prefix=${INSTALL_DIR} CFLAGS="-DRDK_LOGGER" 
 make && make install
