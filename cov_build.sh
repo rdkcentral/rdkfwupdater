81c0862667049824d842fd2852608680bfa3193f
mkadinti <madhunarasimhulu@gmail.com>
2026-02-02 20:18:12 +0000

    RDK-60065:[RDK-E] Implement Firmware Download & Install API for Event-Driven Firmware Updates- UpdateFirmware API Implementation- PR clean up

--- cov_build.sh
+++ cov_build.sh
@@ -3,7 +3,6 @@ export ROOT=/usr
 export INSTALL_DIR=${ROOT}/local
 mkdir -p $INSTALL_DIR
 
-
 # Below dependencies are available in native build containers 
 # 1] utilities
 # 2] libsyswrapper
@@ -11,5 +10,5 @@ mkdir -p $INSTALL_DIR
 
 #Build rdkfwupdater
 autoreconf -i
-./configure --prefix=${INSTALL_DIR} CFLAGS="-DRDK_LOGGER" 
+./configure --prefix=${INSTALL_DIR} CFLAGS="-DRDK_LOGGER"
 make && make install
